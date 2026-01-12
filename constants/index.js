0436533a570adfbd5b46c36c5c88da32e2e62217
Mei Eutsler <mbeutsler@gmail.com>
2026-01-12 09:29:43 -0700

    Implement NavBar and first part of hero section

---
+++ constants/index.js
@@ -0,0 +1,169 @@
+export const navLinks = [
+    {
+        title: 'Cocktails',
+        id: 'cocktails'
+    },
+    {
+        title: 'About Us',
+        id: 'about'
+    },
+    {
+        title: 'The Art',
+        id: 'work'
+    },
+    {
+        title: 'Contact',
+        id: 'contact'
+    },
+];
+
+export const cocktailsList = [
+    {
+        name: "Chapel Hill Shiraz",
+        country: "AU",
+        detail: "Battle",
+        price: "$22"
+    },
+    {
+        name: "Caten Malbee",
+        country: "AU",
+        detail: "Battle",
+        price: "$29"
+    },
+    {
+        name: "Rhino Pale Ale",
+        country: "CA",
+        detail: "750 ml",
+        price: "$18"
+    },
+    {
+        name: "Irish Guinness",
+        country: "IE",
+        detail: "600 ml",
+        price: "$14"
+    },
+];
+
+export const mocktailsList = [
+    {
+        name: "Tropical Bloom",
+        country: "US",
+        detail: "Battle",
+        price: "$12"
+    },
+    {
+        name: "Passionfruit Mint",
+        country: "US",
+        detail: "Battle",
+        price: "$14"
+    },
+    {
+        name: "Citrus Glow",
+        country: "CA",
+        detail: "750 ml",
+        price: "$18"
+    },
+    {
+        name: "Lavender Fizz",
+        country: "IE",
+        detail: "600 ml",
+        price: "$22"
+    },
+];
+
+export const profilesList = [
+    {
+        imgPath: "/images/profile1.png"
+    },
+    {
+        imgPath: "/images/profile2.png"
+    },
+    {
+        imgPath: "/images/profile3.png"
+    },
+    {
+        imgPath: "/images/profile4.png"
+    },
+];
+
+export const featuresList = [
+    "Perfectly balanced blends",
+    "Garnished to perfection",
+    "Ice-cold every time",
+    "Expertly shaken & stirred"
+];
+
+export const goodsList = [
+    "Handpicked ingredients",
+    "Signature techniques",
+    "Bartending artistry in action",
+    "Freshly muddled flavors"
+];
+
+export const storeInfo = {
+    heading: "Where to Find Us",
+    address: "456 Raq Blvd., Suite #404, Los Angeles, CA 90210",
+    contact: {
+        phone: "(555) 987-6543",
+        email: "contact@zestycocktails.com"
+    },
+};
+
+export const openingHours = [
+    { day: "Mon-Thu", time: "11:00am - 12:00am" },
+    { day: "Fri-Sat", time: "9:00am - 2:00am" },
+    { day: "Sun", time: "9:00am - 1:00am" }
+];
+
+export const socials = [
+    {
+        name: "Instagram",
+        icon: "/images/insta.png",
+        url: "#",
+    },
+    {
+        name: "X (Twitter)",
+        icon: "/images/x.png",
+        url: "#",
+    },
+    {
+        name: "Facebook",
+        icon: "/images/fb.png",
+        url: "#",
+    },
+];
+
+export const allCocktails = [
+    {
+        id: 1,
+        name: "Classic Mojito",
+        image: "/images/drink1.png",
+        title: "Simple Ingredients, Bold Flavor",
+        description:
+            "Made with white rum, lime juice, simple syrup and club soda, garnished with Cuban mint. This classic shining in armor can save any bad day."
+    },
+    {
+        id: 2,
+        name: "Raspberry Delight",
+        image: "/images/drink2.png",
+        title: "Crafted With Care, Poured With Love",
+        description:
+            "This mojito is a spin on the classic that dresses gold rum, tangy lime, fresh raspberries, spearmint and lemon-lime soda. Shaken, frozen, or on the rocks—she's a damsel that can take care of herself."
+    },
+    {
+        id: 3,
+        name: "Violet Breeze",
+        image: "/images/drink3.png",
+        title: "Fruity With A Touch of Fire",
+        description:
+            "Balanced with spiced rum, Chambord, lime juice, sweet mint and Fanta orange. This mojito is full of character and wit it will make anyone sharp tongued. Add extra sweet mint for the perfect drink around the table."
+    },
+    {
+        id: 4,
+        name: "Curacao Refresher",
+        image: "/images/drink4.png",
+        title: "A Fan Favorite With A Bite",
+        description:
+            "Curated with blue curacao, lime juice, peppermint and lemon-lime soda. The Curacao Mojito emphasizes on the zesty and mint flavors that make tales so memorable."
+    },
+];
